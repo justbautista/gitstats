c9bcb43a852949f28c1f78bbce3a08e19a7c89bd
justin <justinbautista923@gmail.com>
2023-06-08 19:16:48 -0400

    fix fetch url

--- frontend/src/Results.js
+++ frontend/src/Results.js
@@ -26,7 +26,7 @@ export default function Results({ username, forked }) {
             }
 
             try {
-                const response = await fetch(process.env.BACKEND_URL, {
+                const response = await fetch(`${process.env.BACKEND_URL}/stats`, {
                     method: "POST",
                     headers: {
                         'Content-Type': 'application/json',
